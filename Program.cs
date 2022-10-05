d936b7b983dac0ca8a641c512188260a3385edfa
Karina <nikiforovakarina45@gmail.com>
2022-10-05 12:14:32 +0300

    Добавили write

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿Console.WriteLine("Введите ваше имя ");
+﻿Console.Write("Введите ваше имя ");
 string username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+Console.Write("Привет, ");
+Console.Write(username);
+
 
 
